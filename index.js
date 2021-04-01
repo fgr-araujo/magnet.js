d41885894a842991f45ac6f6a203ff4e94f4d703
Wan Wan <lf2com.bt@gmail.com>
2021-04-01 23:44:59 +0800

    20210401.1

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-'use strict';
-
 import { Magnet, MagnetGroup } from './libs/magnet';
 import MagnetBase from './libs/magnet/base';
 
+Promise.all(
+  [Magnet, MagnetGroup]
+    .map((element) => {
+      if (customElements.get(element.nodeName)) {
+        return Promise.resolve();
+      }
 
-Promise.all([Magnet, MagnetGroup]
-  .map((element) => {
-    if (customElements.get(element.nodeName)) {
-      return Promise.resolve();
-    }
-
-    customElements.define(element.nodeName, element);
-    return customElements.whenDefined(element.nodeName);
-  })
+      customElements.define(element.nodeName, element);
+      return customElements.whenDefined(element.nodeName);
+    }),
 )
   .then(() => {
-    console.log(`Magnet ready!`);
+    // magnet.js ready
   });
+
+const { self } = window;
 
 if (self && self instanceof Object && self === self.self) {
   self.Magnet = Magnet;
-  
+
   // might be useless
   self.MagnetGroup = MagnetGroup;
   self.MagnetBase = MagnetBase;
@@ -30,6 +30,5 @@ export {
   MagnetBase,
   MagnetGroup,
 };
-
 
 export default Magnet;
--- libs/stdlib.js
+++ libs/stdlib.js
@@ -1,22 +1,185 @@
-'use strict';
+/**
+ * Check if {src} is not undefined
+ *
+ * @param {any} src
+ * @returns {boolean}
+ */
+export const isset = (src) => src !== undefined;
 
-export const isset = (o) => 'undefined' !== typeof o;
-export const useor = (a, b, f = isset) => f(a) ?a :b;
-export const isbool = (b) => 'boolean' === typeof b;
-export const tobool = (b) => Boolean(b);
-export const isnum = (n) => !isNaN(n);
-export const tonum = (n) => parseFloat(n);
-export const isint = (n) => isnum(n) && n === n | 0;
-export const isstr = (s) => 'string' === typeof s || (isset(s) && s instanceof String);
-export const tostr = (s) => String(s);
-export const isfunc = (f) => 'function' === typeof f;
-export const isarray = (a) => Array.isArray(a);
-export const arrayable = (a) => isset(a) && isint(a.length) && 0 <= a.length;
-export const toarray = (a) => Array.from(a);
-export const objKeys = (o) => Object.keys(o);
-export const objForEach = (o, f = ()=>{}, t = this) => objKeys(o).forEach((k) => f.call(t, o[k], k, o));
-export const objReduce = (o, f = ()=>{}, s) => objKeys(o).reduce((s, k) => f(s, o[k], k, o), s);
-export const objMap = (o, f = ()=>{}, t = this) => objReduce(o, (s, v, k) => ({ ...s, [k]: f.call(t, v, k, o) }), {});
-export const objValues = (o) => objReduce(o, (a, v) => a.concat(v), []);
-export const iselem = (e) => e instanceof Element || e instanceof Window || e instanceof Document;
-export const getStyle = (d) => d.currentStyle || window.getComputedStyle(d) || d.style;
+/**
+ * Return {a} if it passed {validator}, or return {b}
+ *
+ * @param {any} a
+ * @param {any} b
+ * @param {function} validator
+ * @returns {a|b}
+ */
+export const useor = (a, b, validator = isset) => (
+  validator(a) ? a : b
+);
+
+/**
+ * Check if {src} is boolean
+ *
+ * @param {any} src
+ * @returns {boolean}
+ */
+export const isbool = (src) => typeof src === 'boolean';
+
+/**
+ * Convert {src} to boolean
+ *
+ * @param {any} src
+ * @returns {boolean}
+ */
+export const tobool = (src) => Boolean(src);
+
+/**
+ * Check if {src} is number
+ *
+ * @param {any} src
+ * @returns {number}
+ */
+export const isnum = (src) => !Number.isNaN(src);
+
+/**
+ * Convert {src} to number
+ * @param {any} src
+ * @returns {number}
+ */
+export const tonum = (src) => parseFloat(src);
+
+/**
+ * Check if {src} is integer
+ *
+ * @param {any} src
+ * @returns {boolean}
+ */
+// eslint-disable-next-line no-bitwise
+export const isint = (src) => isnum(src) && src === (src | 0);
+
+/**
+ * Check if {src} is string
+ *
+ * @param {any} src
+ * @returns {boolean}
+ */
+export const isstr = (src) => typeof src === 'string' || (isset(src) && src instanceof String);
+
+/**
+ * Convert {src} to string
+ *
+ * @param {any} src
+ * @returns {string}
+ */
+export const tostr = (src) => String(src);
+
+/**
+ * Check if {src} is function
+ *
+ * @param {any} src
+ * @returns {boolean}
+ */
+export const isfunc = (src) => typeof src === 'function';
+
+/**
+ * Check if {src} is array
+ *
+ * @param {any} src
+ * @returns {boolean}
+ */
+export const isarray = (src) => Array.isArray(src);
+
+/**
+ * Check if {src} is able to be array
+ *
+ * @param {any} src
+ * @returns {boolean}
+ */
+export const arrayable = (src) => isset(src) && isint(src.length) && src.length >= 0;
+
+/**
+ * Convert {src} to array
+ *
+ * @param {any} src
+ * @returns {array}
+ */
+export const toarray = (src) => Array.from(src);
+
+/**
+ * Get keys of {obj}
+ *
+ * @param {object} obj
+ * @returns {array}
+ */
+export const objKeys = (obj) => Object.keys(obj);
+
+/**
+ * Get values of {obj}
+ *
+ * @param {object} obj
+ * @returns {array}
+ */
+export const objValues = (obj) => objKeys(obj).map((key) => obj[key]);
+
+/**
+ * Traverse values of {obj}
+ *
+ * @param {object} obj
+ * @param {function} func
+ * @param {any} self as this for {func}
+ * @returns {void}
+ */
+export const objForEach = (obj, func = () => {}, self = this) => (
+  objKeys(obj).forEach((key) => func.call(self, obj[key], key, obj))
+);
+
+/**
+ * Traverse values of {obj} with passing {initial}
+ * @param {object} obj
+ * @param {function} func
+ * @param {any} initial
+ * @returns {any} passed by {func}
+ */
+export const objReduce = (obj, func = () => {}, initial) => (
+  objKeys(obj).reduce((past, key) => func(past, obj[key], key, obj), initial)
+);
+
+/**
+ * Fill object with keys of {obj} with result of {func}
+ *
+ * @param {object} obj
+ * @param {function} func
+ * @param {any} self
+ * @returns {object}
+ */
+export const objMap = (obj, func = () => {}, self = this) => (
+  objReduce(obj, (past, val, key) => ({
+    ...past,
+    [key]: func.call(self, val, key, obj),
+  }), {})
+);
+
+/**
+ * Check if {src} is element
+ *
+ * @param {any} src
+ * @returns {boolean}
+ */
+export const iselem = (src) => (
+  src instanceof Element
+  || src instanceof Window
+  || src instanceof Document
+);
+
+/**
+ * Get style of {dom}
+ *
+ * @param {DOM} dom
+ * @returns {object} of style
+ */
+export const getStyle = (dom) => (
+  dom.currentStyle
+  || window?.getComputedStyle(dom)
+  || dom.style
+);
